f64d0b7f0037437e79d3951d9aa5187b1666fb51
Tom Ribbens <tom@tomribbens.be>
2026-08-03 01:54:44 +0200

    Import Compose list item helper

--- app/src/main/java/com/example/hanabihinttracker/MainActivity.kt
+++ app/src/main/java/com/example/hanabihinttracker/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
