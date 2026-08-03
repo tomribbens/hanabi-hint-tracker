697e2f2d36330371023bba00ebee51b2b56215ac
Tom Ribbens <tom@tomribbens.be>
2026-08-03 04:22:06 +0200

    Fix gesture import

--- app/src/main/java/com/example/hanabihinttracker/MainActivity.kt
+++ app/src/main/java/com/example/hanabihinttracker/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.awaitLongPressOrCancellation
 import androidx.compose.foundation.gestures.drag
 import androidx.compose.foundation.layout.*
@@ -30,7 +31,6 @@ import androidx.compose.ui.graphics.Color as UiColor
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.input.pointer.PointerEventPass
-import androidx.compose.ui.input.pointer.awaitFirstDown
 import androidx.compose.ui.input.pointer.positionChange
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.text.font.FontWeight
